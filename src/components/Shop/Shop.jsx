a0c2016f3c046352a92e65de8053cca5698ce955
engrnazrulislam <nazrul.cmt12@gmail.com>
2023-08-30 21:01:27 +0600

    show cart data

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -13,22 +13,42 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[]);
     const handleAddToCart=(product)=>{
-        const newCart=[...cart,product];
+        //cart.push(product)
+        let newCart=[];
+        // const newCart=[...cart,product];
+        //If product doesn't exists in the cart, then set quantity=1
+        //If product exists, update the quantity by 1
+        const exists=cart.find(pd => pd.id===product.id);
+        if(!exists){
+            product.quantity=1;
+            newCart=[...cart,product];
+        }
+        else{
+            exists.quantity=exists.quantity+1;
+            const remaining=cart.filter(pd => pd.id !==product.id);
+            newCart=[...remaining,exists];
+        }
+
         setCart(newCart);
         addToDb(product.id);
     }
     useEffect(()=>{
         const storedCart=getShoppingCart();
-        //Step 1: get id
+        const savedCart=[];
+        //step 1: get id of the addedProduct
         for(const id in storedCart){
-            //step 2 get the product by using id
-            const addedProduct = products.find(product => product.id ===id);
-            //step 3: get quantity of the product
-            const quantity = storedCart[id];
-            addedProduct.quantity=quantity;
-            console.log(addedProduct);
+            //step2: get product from products state by using id
+            const addedProduct=products.find(product => product.id===id);
+            if(addedProduct){
+                //step3: add quantity
+                const quantity=storedCart[id];
+                addedProduct.quantity=quantity;
+                savedCart.push(addedProduct);
+            }
+            console.log('Added Product',addedProduct);
         }
-    },[])
+        setCart(savedCart);
+    },[products])
     return (
         <div className='shop-container'>
             <div className='products-container'>
